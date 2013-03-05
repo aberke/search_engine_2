db614a54357c2872cd2fe11bf290bdb64f459808
Matt Patenaude <matt@mattpatenaude.com>
2013-03-05 13:35:56 -0500

    Added function prototypes to stemmer.c to quiet some gcc warnings

--- searchio/stemmer.c
+++ searchio/stemmer.c
@@ -43,6 +43,23 @@
 static char * b;       /* buffer for word to be stemmed */
 static int k,k0,j;     /* j is a general offset into the string */
 
+
+/* function prototypes to quiet compiler warnings */
+static int cons(int i);
+static int m();
+static int vowelinstem();
+static int doublec(int j);
+static int cvc(int i);
+static int ends(char * s);
+static void setto(char * s);
+static void r(char * s);
+static void step1ab();
+static void step1c();
+static void step2();
+static void step3();
+static void step4();
+static void step5();
+
 /* cons(i) is TRUE <=> b[i] is a consonant. */
 
 static int cons(int i)
